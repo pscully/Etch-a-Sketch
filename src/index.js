0ab4688fe960ae7301802e041c07e12f4e134c2d
Patrick Scully <10429618+pscully@users.noreply.github.com>
2019-05-23 22:26:31 -0400

    Add grid and mouseover effect; set up initial buttons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,131 @@
-import "./styles.css";
+let number = 16;
+let colorChange = false;
+let globalColor = '#000000';
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+let app,
+  buttonClear,
+  buttonColor,
+  buttonReset,
+  buttonSection,
+  container,
+  gridCell,
+  gridWrapper,
+  section,
+  title;
+
+const colors = [
+  '#7DB32B',
+  '#A41809',
+  '#421AAE',
+  '#FEFEE3',
+  '#F91A84',
+  '#D01943',
+  '#4E8669',
+  '#FBB065',
+  '#F53F42',
+  '#23B4E4',
+  '#8C5959',
+  '#D9860F',
+  '#FFAB01',
+  '#1F64EB',
+  '#71F35E',
+  '#7EAD80'
+];
+
+app = document.querySelector('#app');
+
+buttonClear = create('buttonClear', 'button');
+buttonColor = create('buttonColor', 'button');
+buttonReset = create('buttonReset', 'button');
+buttonSection = create('buttonSection', 'section');
+container = create('container', 'div');
+gridWrapper = create('gridWrapper', 'div');
+section = create('section', 'section');
+
+buttonClear.innerText = 'CLEAR';
+buttonColor.innerText = 'COLOR';
+buttonReset.innerText = 'RESTART';
+
+app.appendChild(container);
+container.appendChild(section);
+container.appendChild(buttonSection);
+buttonSection.appendChild(buttonColor);
+buttonSection.appendChild(buttonClear);
+buttonSection.appendChild(buttonReset);
+container.appendChild(gridWrapper);
+
+gridWrapper.classList.add('wrapper', 'container');
+buttonClear.classList.add('button');
+buttonColor.classList.add('button');
+buttonReset.classList.add('button');
+buttonSection.classList.add('button-container');
+
+buttonClear.addEventListener('click', function() {
+  resetTable();
+});
+
+buttonColor.addEventListener('click', function() {
+  flip(colorChange);
+  pickColor();
+});
+
+buttonReset.addEventListener('click', function() {
+  number = prompt('How many squares do you want?', '16');
+  createGrid(number);
+});
+
+gridWrapper.addEventListener('mouseover', function(e) {
+  if (e.target.classList.contains('grid')) {
+    e.target.style.backgroundColor = globalColor;
+  }
+});
+
+function createHeader() {
+  title = create('title', 'h1');
+  title.classList.add('title');
+  title.innerText = 'Etch a Sketch';
+  section.appendChild(title);
+  return;
+}
+
+function createGrid(number) {
+  const total = number * number;
+  for (var i = 1; i <= total; i++) {
+    gridCell = create('gridCell', 'div');
+    gridCell.setAttribute('id', `cell-${i}`);
+    gridWrapper.appendChild(gridCell).classList.add(`grid`);
+  }
+}
+
+function resetTable() {
+  let gridCells = document.getElementsByClassName('grid');
+  while (gridCells[0]) {
+    gridCells[0].parentNode.removeChild(gridCells[0]);
+  }
+}
+
+function pickColor() {
+  let i = getRandomInt(colors.length);
+  let color = colors[i];
+  buttonColor.style.backgroundColor = color;
+  globalColor = color;
+}
+
+function flip(boolVariable) {
+  if (boolVariable) {
+    boolVariable = false;
+  } else {
+    boolVariable = true;
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function create(variable, type) {
+  return document.createElement(`${type}`);
+}
+
+createHeader();
+createGrid(number);
